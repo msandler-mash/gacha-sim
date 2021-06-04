0ad135add45d669f3712872fe04f55f1ac96c517
Matthew Sandler <matthewhsandler@yahoo.com>
2021-06-04 18:24:39 -0400

    We did it we might have made a functioning gacha
    updated the gatcha spinning mechanics, as well as added priconne story banner as an example

--- GatchaGame/app/src/main/java/com/example/gatchagame/GatchaSim.kt
+++ GatchaGame/app/src/main/java/com/example/gatchagame/GatchaSim.kt
@@ -1,23 +1,46 @@
 package com.example.gatchagame
 
 import java.util.ArrayList
+import kotlin.random.Random
 
 
-class GatchaSim (val names: ArrayList<String>, val rates: ArrayList<Float> ){
+class GatchaSim (val names: ArrayList<String>, val rates: ArrayList<Int> ) {
     lateinit var gatcha: ArrayList<Character>
-    fun makeGatcha(){
+    fun makeGatcha() {
+        gatcha = ArrayList<Character>()
         var i = 0
-        while(i < names.size){
+        while (i < names.size) {
             var toAdd: Character
-            if(i == 0){
-                toAdd = Character(names.get(i), rates.get(i))
+            if (i == 0) {
+                toAdd = Character(names.get(i), 0)
             } else {
-                toAdd = Character(names.get(i), rates.get(i) + rates.get(i-1))
+                toAdd = Character(names.get(i), rates.get(i) + gatcha.get(i - 1).rate)
             }
             gatcha.add(toAdd);
+            i++
         }
     }
 
-    class Character (val name: String, val rate: Float){
+    fun spin(): Character {
+        var roll = Random.nextInt(0,1000000)
+        var i = 0
+        lateinit var toReturn: Character
+        while (i < names.size) {
+            if (gatcha.get(i).rate < roll) {
+                if (i == names.size - 1) {
+                    toReturn = gatcha.get(i)
+                    break
+                }
+                if (gatcha.get(i + 1).rate > roll) {
+                    toReturn = gatcha.get(i)
+                    break
+                }
+            }
+            i++
+        }
+        return toReturn
+    }
+
+    class Character(val name: String, val rate: Int) {
     }
 }
--- GatchaGame/app/src/main/java/com/example/gatchagame/PriconeSim.kt
+++ GatchaGame/app/src/main/java/com/example/gatchagame/PriconeSim.kt
@@ -7,11 +7,85 @@ import android.widget.Button
 import android.widget.TextView
 
 class PriconeSim : Activity(){
+    lateinit var storyGatcha: GatchaSim
+
+    val THREE_STAR_STORY_RATE = 1389
+    val THREE_STAR_STORY_RATE2 = 1388
+    val THREE_STAR_BOOST_RATE = 7000
+    val THREE_STAR_NO_BOOST_RATE = 1059
+    val THREE_STAR_NO_BOOST_RATE2 = 1058
+    val TWO_STAR_STORY_RATE = 9474
+    val TWO_STAR_STORY_RATE2 = 9473
+    val TWO_STAR_BOOST_RATE = 0
+    val TWO_STAR_NO_BOOST_RATE = 0
+    val TWO_STAR_10_ROLL_RATE = 51316
+    val TWO_STAR_10_ROLL_RATE2 = 51315
+    val TWO_STAR_10_ROLL_BOOST_RATE = 0
+    val ONE_STAR_RATE = 79500
+
+    val THREE_STAR_POOL_STORY = listOf("Anna", "Maho", "Rino", "Hatsune", "Io", "Saren", "Nozomi", "Ninon", "Akino",
+    "Makoto", "Shizuru", "Monika", "Djeeta", "Jun", "Arisa", "Kyoka")
+    val THREE_STAR_POOL_STORY2 = listOf("Illya", "S. Pecorine")
+
+    val TWO_STAR_POOL_STORY = listOf("Akari", "Miyako", "Yuki", "Suzuna", "Kaori", "Mimi", "Ayane", "Eriko", "Shinobu", "Mahiru",
+    "Shiori", "Chika", "Kuka")
+    val TWO_STAR_POOL_STORY2 = listOf("Tamaki", "Mifuyu", "Mitsuki", "Rin", "Misato", "Tsumugi")
+
+    val ONE_STAR_POOL_STORY = listOf("Hiyori", "Rei", "Misogi", "Kurumi", "Yori", "Suzume", "Yukari", "Aoi", "Misaki", "Lima")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.spin_layout)
 
         var titleText = findViewById<TextView>(R.id.gameTitle);
         titleText.text = "Princess Connect"
+
+        setupStoryGatcha()
+        var resultsText = findViewById<TextView>(R.id.resultsText)
+        var singleSummonButton = findViewById<Button>(R.id.singleSummon)
+        singleSummonButton.setOnClickListener {
+            var result = storyGatcha.spin()
+            resultsText.text = result.name
+
+        }
+
+    }
+
+    fun setupStoryGatcha(){
+        var namePool = ArrayList<String>()
+        var ratePool = ArrayList<Int>()
+        var i = 0
+
+        while(i < THREE_STAR_POOL_STORY.size){
+            namePool.add(THREE_STAR_POOL_STORY.get(i))
+            ratePool.add(THREE_STAR_STORY_RATE)
+            i++
+        }
+        i=0
+        while(i < THREE_STAR_POOL_STORY2.size){
+            namePool.add(THREE_STAR_POOL_STORY2.get(i))
+            ratePool.add(THREE_STAR_STORY_RATE2)
+            i++
+        }
+        i=0
+        while(i < TWO_STAR_POOL_STORY.size){
+            namePool.add(TWO_STAR_POOL_STORY.get(i))
+            ratePool.add(TWO_STAR_STORY_RATE)
+            i++
+        }
+        i=0
+        while(i < TWO_STAR_POOL_STORY2.size){
+            namePool.add(TWO_STAR_POOL_STORY2.get(i))
+            ratePool.add(TWO_STAR_STORY_RATE2)
+            i++
+        }
+        i=0
+        while(i < ONE_STAR_POOL_STORY.size){
+            namePool.add(ONE_STAR_POOL_STORY.get(i))
+            ratePool.add(ONE_STAR_RATE)
+            i++
+        }
+
+        storyGatcha = GatchaSim(namePool, ratePool)
+        storyGatcha.makeGatcha()
     }
 }
